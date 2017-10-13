d1ba0c2f5e3da1aa6700caef95370ddf403abacb
Jason Bean <taddeusz@gmail.com>
2017-10-13 18:02:17 -0500

    test

--- godaddy-dyndns/setcron.sh
+++ godaddy-dyndns/setcron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CRONTAB_ENTRY=$(printf "*/%s * * * * /godaddy-dyndns/godaddy-dyndns.sh /config" "${FREQ_MINUTES}")
+CRONTAB_ENTRY=$(printf "*/%s * * * * /godaddy-dyndns/godaddy-dyndns.sh /config" "$FREQ_MINUTES")
 
 crontab -r
 (crontab -l 2>/dev/null; echo "$CRONTAB_ENTRY") | crontab -
