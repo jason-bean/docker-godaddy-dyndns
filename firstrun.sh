10f487df15dec042d05cdbcaf053650f760711d4
Jason Bean <taddeusz@gmail.com>
2017-10-13 10:06:57 -0500

    Fix firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -4,5 +4,5 @@ if [ -f /godaddy-dyndns/godaddy-dyndns.py ]; then
   echo "godaddy-dyndns files already installed."
 else
   echo "Installing godaddy-dyndns files."
-  copy /etc/firstrun/godaddy-dyndns/* /godaddy-dyndns
+  cp /etc/firstrun/godaddy-dyndns/* /godaddy-dyndns
 fi
