51d374f02a2dc21f385f4f0d271dc4e9898fb77c
Jason Bean <taddeusz@gmail.com>
2017-11-15 15:24:35 -0600

    Changed to Alpine 3.6 and use tini for init rather than supervisord.

--- image/startup.sh
+++ image/startup.sh
@@ -4,3 +4,5 @@ if [ ! -f /config/godaddy-dyndns.conf.template ]; then
   echo "Copy template file."
   cp /godaddy-dyndns/godaddy-dyndns.conf.template /config
 fi
+
+exec /sbin/tini -- crond -f -l 8
